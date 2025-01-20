7222af9c5e7c145c22cbd37b36da711fd5e08b3a
Facundo Rozalez <facurozalez@gmail.com>
2025-01-19 23:32:08 -0300

    agregue la carpeta array

---
+++ jschallenger/javascript_array/01-Get nth element of array.js
@@ -0,0 +1,12 @@
+//Write a function that takes an array (a) and a value (n) as argument. Return the nth element of 'a'//
+
+//Escribe una funcion
+//que tome un arreglo (a) y un valor (n) como argumento
+// retorna el nth elemento de 'a'
+
+function myfunction (a,n){
+
+    return a[n-1]
+}
+
+console.log(myfunction [1, 2, 3, 4],3)
